a288b50a7cc4a79b23d48b23d177bf07de572bd0
Deva Pal <devapal845@gmail.com>
2026-03-29 18:51:54 +0530

    Connected frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+ baseURL: "https://disaster-management-system-chi7.onrender.com",
 });
 
 // 🔥 AUTO ATTACH TOKEN
--- frontend/src/services/socket.js
+++ frontend/src/services/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000", {
+const socket = io( "https://disaster-management-system-chi7.onrender.com", {
   transports: ["websocket"],
 });
 
