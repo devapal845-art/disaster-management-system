948c4eb5d014f09164a30da6e66b9b5315931194
Deva Pal <devapal845@gmail.com>
2026-03-30 23:13:48 +0530

    your message here

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,36 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
-// 🔥 GLOBAL FIX (runs once for entire app)
-delete L.Icon.Default.prototype._getIconUrl;
-
+// 🔥 FINAL FIX (Vite + Render compatible)
 L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
-  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
-  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+  iconRetinaUrl: new URL(
+    "leaflet/dist/images/marker-icon-2x.png",
+    import.meta.url
+  ).href,
+  iconUrl: new URL(
+    "leaflet/dist/images/marker-icon.png",
+    import.meta.url
+  ).href,
+  shadowUrl: new URL(
+    "leaflet/dist/images/marker-shadow.png",
+    import.meta.url
+  ).href,
 });
-import App from "./App.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
