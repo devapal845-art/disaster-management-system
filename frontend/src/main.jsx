c07ddd53cf3b7d0662a06ef4c70ad57502ab767d
Deva Pal <devapal845@gmail.com>
2026-03-30 23:00:44 +0530

    your message here

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+// 🔥 GLOBAL FIX (runs once for entire app)
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
